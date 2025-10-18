6552177c4811362dcba2b2cf7c8e194ba03d873e
Mo <moh.goodarzi2001@gmail.com>
2025-10-18 18:27:15 +0200

    create main.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,8 +1,10 @@
 import os
 from dotenv import load_dotenv
 from pinecone import Pinecone
-load_dotenv()
 from openai import OpenAI
+
+load_dotenv()
+
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
@@ -12,21 +14,19 @@ index = pc.Index(os.getenv("INDEX_NAME"))
 
 
 def create_embeddings(inputs):
-    response = client.embeddings.create(
-        model="text-embedding-3-small",
-        input=inputs
-    )
+    response = client.embeddings.create(model="text-embedding-3-small", input=inputs)
     return [item.embedding for item in response.data]
 
 
-def query_embedding(input_embedding, top_k=10, namespace="namespace_until_1990", movie_name=None):
-    
+def query_embedding(
+    input_embedding, top_k=10, namespace="namespace_until_1990", movie_name=None
+):
 
     response = index.query(
         vector=input_embedding,
         top_k=top_k,
         include_metadata=True,
         filter={"title": {"$ne": movie_name}},
-        namespace=namespace
+        namespace=namespace,
     )
     return response
